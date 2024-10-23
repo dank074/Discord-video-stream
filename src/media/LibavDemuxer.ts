9f824feeb1fb51bd9525b1b006338a16c5ae37a9
Long Nguyen <nguyen.long.908132@gmail.com>
2024-10-23 14:42:24 +0700

    Synthesize parameter sets NAL units
    It turns out, the `mp4_h264toannexb` filter was helping us a great deal here, by inserting the parameter sets before each I frame. With mkv and other containers, they're only specified once in the file, so we need to synthesize it ourselves

--- src/media/LibavDemuxer.ts
+++ src/media/LibavDemuxer.ts
@@ -1,8 +1,32 @@
 import LibAV from "@libav.js/variant-webcodecs";
 import { uid } from "uid";
 import { AVCodecID } from "./LibavCodecId.js";
+import {
+    H264Helpers, H264NalUnitTypes,
+    H265Helpers, H265NalUnitTypes,
+    splitNalu, mergeNalu
+} from "../client/processing/AnnexBHelper.js";
 import { PassThrough } from "node:stream";
-import type { Readable } from "node:stream";
+import type { Readable, Transform } from "node:stream";
+
+type MediaStreamInfoCommon = {
+    index: number,
+    codec: AVCodecID,
+    stream: Transform
+}
+type VideoStreamInfo = MediaStreamInfoCommon & {
+    width: number,
+    height: number,
+    framerate_num: number,
+    framerate_den: number,
+    extradata?: unknown
+}
+type AudioStreamInfo = MediaStreamInfoCommon & {
+    sample_rate: number
+};
+
+type H264ParamSets = Record<"sps" | "pps", Buffer[]>
+type H265ParamSets = Record<"vps" | "sps" | "pps", Buffer[]>
 
 let libavPromise: Promise<LibAV.LibAV>;
 
@@ -17,6 +41,140 @@ const allowedVideoCodec = new Set([
 const allowedAudioCodec = new Set([
     AVCodecID.AV_CODEC_ID_OPUS
 ]);
+
+// Parse the avcC atom, which contains SPS and PPS
+function parseavcC(input: Buffer) {
+    if (input[0] != 1)
+        throw new Error("Only configurationVersion 1 is supported");
+    // Skip a bunch of stuff we don't care about
+    input = input.subarray(5);
+
+    const sps: Buffer[] = [];
+    const pps: Buffer[] = [];
+
+    // Read the SPS
+    const spsCount = input[0] & 0b11111;
+    input = input.subarray(1);
+    for (let i = 0; i < spsCount; ++i) {
+        const spsLength = input.readUInt16BE();
+        input = input.subarray(2);
+        sps.push(input.subarray(0, spsLength));
+        input = input.subarray(spsLength);
+    }
+
+    // Read the PPS
+    const ppsCount = input[0];
+    input = input.subarray(1);
+    for (let i = 0; i < ppsCount; ++i) {
+        const ppsLength = input.readUInt16BE();
+        input = input.subarray(2);
+        pps.push(input.subarray(0, ppsLength));
+        input = input.subarray(ppsLength);
+    }
+    return { sps, pps }
+}
+
+// Parse the hvcC atom, which contains VPS, SPS, PPS
+function parsehvcC(input: Buffer) {
+    if (input[0] != 1)
+        throw new Error("Only configurationVersion 1 is supported");
+    // Skip a bunch of stuff we don't care about
+    input = input.subarray(22);
+
+    const vps: Buffer[] = [];
+    const sps: Buffer[] = [];
+    const pps: Buffer[] = [];
+
+    const numOfArrays = input[0];
+    input = input.subarray(1);
+
+    for (let i = 0; i < numOfArrays; ++i) {
+        const naluType = input[0] & 0b111111;
+        input = input.subarray(1);
+
+        const naluCount = input.readUInt16BE();
+        input = input.subarray(2);
+
+        for (let j = 0; j < naluCount; ++j) {
+            const naluLength = input.readUInt16BE();
+            input = input.subarray(2);
+
+            const nalu = input.subarray(0, naluLength);
+            input = input.subarray(naluLength);
+
+            if (naluType == H265NalUnitTypes.VPS_NUT)
+                vps.push(nalu);
+            else if (naluType == H265NalUnitTypes.SPS_NUT)
+                sps.push(nalu);
+            else if (naluType == H265NalUnitTypes.PPS_NUT)
+                pps.push(nalu);
+        }
+    }
+    return { vps, sps, pps }
+}
+
+function h264AddParamSets(frame: Buffer, paramSets: H264ParamSets) {
+    const { sps, pps } = paramSets;
+    const nalus = splitNalu(frame);
+    // Technically non-IDR I frames exist ("open GOP"), but they're exceedingly
+    // rare in the wild, and no encoder produces it by default
+    let isIDR = false;
+    let hasSPS = false;
+    let hasPPS = false;
+    for (const nalu of nalus) {
+        const naluType = H264Helpers.getUnitType(nalu);
+        if (naluType == H264NalUnitTypes.CodedSliceIdr)
+            isIDR = true;
+        else if (naluType == H264NalUnitTypes.SPS)
+            hasSPS = true;
+        else if (naluType == H264NalUnitTypes.PPS)
+            hasPPS = true;
+    }
+    if (!isIDR) {
+        // Not an IDR, return as is
+        return frame;
+    }
+    const chunks = [];
+    if (!hasPPS)
+        chunks.push(...sps);
+    if (!hasSPS)
+        chunks.push(...pps);
+    return mergeNalu([...chunks, ...nalus]);
+}
+
+function h265AddParamSets(frame: Buffer, paramSets: H265ParamSets) {
+    const { vps, sps, pps } = paramSets;
+    const nalus = splitNalu(frame);
+    // Technically non-IDR I frames exist ("open GOP"), but they're exceedingly
+    // rare in the wild, and no encoder produces it by default
+    let isIDR = false;
+    let hasVPS = false;
+    let hasSPS = false;
+    let hasPPS = false;
+    for (const nalu of nalus) {
+        const naluType = H265Helpers.getUnitType(nalu);
+        if (naluType == H265NalUnitTypes.IDR_N_LP || naluType == H265NalUnitTypes.IDR_W_RADL)
+            isIDR = true;
+        else if (naluType == H265NalUnitTypes.VPS_NUT)
+            hasVPS = true;
+        else if (naluType == H265NalUnitTypes.SPS_NUT)
+            hasSPS = true;
+        else if (naluType == H265NalUnitTypes.PPS_NUT)
+            hasPPS = true;
+    }
+    if (!isIDR) {
+        // Not an IDR, return as is
+        return frame;
+    }
+    const chunks = [];
+    if (!hasVPS)
+        chunks.push(...vps);
+    if (!hasPPS)
+        chunks.push(...sps);
+    if (!hasSPS)
+        chunks.push(...pps);
+    return mergeNalu([...chunks, ...nalus]);
+}
 
 export async function demux(input: Readable) {
     if (!libavPromise)
@@ -44,11 +202,11 @@ export async function demux(input: Readable) {
 
     const vStream = streams.find((stream) => stream.codec_type == libav.AVMEDIA_TYPE_VIDEO)
     const aStream = streams.find((stream) => stream.codec_type == libav.AVMEDIA_TYPE_AUDIO)
-    let vInfo = null, aInfo = null;
+    let vInfo: VideoStreamInfo | undefined
+    let aInfo: AudioStreamInfo | undefined;
 
     if (vStream) {
-        if (!allowedVideoCodec.has(vStream.codec_id))
-        {
+        if (!allowedVideoCodec.has(vStream.codec_id)) {
             const codecName = await libav.avcodec_get_name(vStream.codec_id);
             cleanup();
             throw new Error(`Video codec ${codecName} is not allowed`)
@@ -62,10 +220,23 @@ export async function demux(input: Readable) {
             framerate_den: await libav.AVCodecParameters_framerate_den(vStream.codecpar),
             stream: new PassThrough({ objectMode: true })
         }
+        if (vStream.codec_id == AVCodecID.AV_CODEC_ID_H264) {
+            const { extradata } = await libav.ff_copyout_codecpar(vStream.codecpar);
+            vInfo = {
+                ...vInfo,
+                extradata: parseavcC(Buffer.from(extradata!))
+            }
+        }
+        else if (vStream.codec_id == AVCodecID.AV_CODEC_ID_H265) {
+            const { extradata } = await libav.ff_copyout_codecpar(vStream.codecpar);
+            vInfo = {
+                ...vInfo,
+                extradata: parsehvcC(Buffer.from(extradata!))
+            }
+        }
     }
     if (aStream) {
-        if (!allowedAudioCodec.has(aStream.codec_id))
-        {
+        if (!allowedAudioCodec.has(aStream.codec_id)) {
             const codecName = await libav.avcodec_get_name(aStream.codec_id);
             cleanup();
             throw new Error(`Audio codec ${codecName} is not allowed`);
@@ -79,16 +250,27 @@ export async function demux(input: Readable) {
     }
 
     (async () => {
-        while (true)
-        {
+        while (true) {
             const [status, streams] = await libav.ff_read_frame_multi(fmt_ctx, pkt, {
                 limit: 16 * 1024,
                 unify: true
             });
-            for (const packet of streams[0])
-            {
-                if (vInfo && vInfo.index === packet.stream_index)
+            for (const packet of streams[0]) {
+                if (vInfo && vInfo.index === packet.stream_index) {
+                    if (vInfo.codec === AVCodecID.AV_CODEC_ID_H264) {
+                        packet.data = h264AddParamSets(
+                            Buffer.from(packet.data),
+                            vInfo.extradata! as H264ParamSets
+                        );
+                    }
+                    else if (vInfo.codec === AVCodecID.AV_CODEC_ID_H265) {
+                        packet.data = h265AddParamSets(
+                            Buffer.from(packet.data),
+                            vInfo.extradata! as H265ParamSets
+                        );
+                    }
                     vInfo.stream.push(packet);
+                }
                 else if (aInfo && aInfo.index === packet.stream_index)
                     aInfo.stream.push(packet);
             }
