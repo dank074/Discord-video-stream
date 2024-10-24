e3250fcca64861f6d3e698231f8ae92273c11386
Long Nguyen <nguyen.long.908132@gmail.com>
2024-10-24 14:24:10 +0700

    Use libav.js in-built function for timestamp conversion
    Avoiding `BigInt` if possible is always good

--- src/media/utils.ts
+++ src/media/utils.ts
@@ -1,14 +1,7 @@
-export function combineLoHi(hi: number, lo: number)
+import LibAV from "@libav.js/variant-webcodecs";
+
+export function combineLoHi(hi: number, lo: number): number
 {
-    let hi_big = BigInt(hi);
-    let lo_big = BigInt(lo);
-    if (hi_big < 0)
-        hi_big += 1n << 32n;
-    let result = (hi_big << 32n) | (lo_big & 0xFFFFFFFFn);
-
-    if (result >= (1n << 63n))
-        result -= 1n << 64n;
-
-    // Let's hope the timestamp won't exceed 2^53 - 1...
-    return Number(result);
+    // @ts-expect-error
+    return LibAV.i64tof64(lo, hi);
 }
